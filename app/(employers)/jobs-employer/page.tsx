c991bf5192afd57c224e1da1aad9fe27694efafb
Sean <thaison@react-plus.com>
2023-02-28 09:04:08 +0700

    fix reload page

--- app/(employers)/jobs-employer/page.tsx
+++ app/(employers)/jobs-employer/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 import { PlusIcon } from '@heroicons/react/24/solid';
 
@@ -12,10 +11,6 @@ import { LJobDetail } from '../../../types/jobsList';
 
 export default function JobDetails() {
   const router = useRouter();
-
-  useEffect(() => {
-    router.refresh();
-  }, []);
 
   const handleRouter = () => {
     router.push('/jobs-employer/job-registration');
--- app/(public-user)/auth/login/page.tsx
+++ app/(public-user)/auth/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { setCookie } from 'cookies-next';
 import { Formik } from 'formik';
@@ -47,10 +47,6 @@ export default function Login() {
       });
     }
   };
-
-  useEffect(() => {
-    router.refresh();
-  }, []);
 
   return (
     <>

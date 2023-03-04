dbe2f23ee9f71a50de0b67c58b4403cb5a7ebce7
Sean <thaison@react-plus.com>
2023-03-04 17:39:32 +0700

    integrate api job detail employer

--- utils/getDay.ts
+++ utils/getDay.ts
@@ -1,13 +1,23 @@
+import 'dayjs/locale/ja';
+
 import dayjs, { Dayjs } from 'dayjs';
+
+dayjs.locale('ja');
+dayjs().locale('ja');
 
 export const getDay = (day: Dayjs) => {
   const daysOfWeek = dayjs.weekdaysShort();
   return daysOfWeek[dayjs(day, 'YYYY-MM-DD').day()];
 };
 
-
 export const getMonthDayCardJob = (day: Dayjs) => {
   const monthDayFormat = dayjs(day).format('MM/DD');
 
   return `${monthDayFormat}`;
+};
+
+export const getTimeCardJob = (day: Dayjs) => {
+  const timeFormat = dayjs(day).format('hh:mm');
+
+  return `${timeFormat}`;
 };

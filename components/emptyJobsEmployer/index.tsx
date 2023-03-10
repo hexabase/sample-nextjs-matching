ece35c3c6393c3babca4c3abd40c5c7f9de6a87a
Sean <thaison@react-plus.com>
2023-03-10 10:18:51 +0700

    add navigation button

--- components/emptyJobsEmployer/index.tsx
+++ components/emptyJobsEmployer/index.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
-import ImageEmpty from '../../public/images/Empty.svg';
+import Link from 'next/link';
+
 import { PlusIcon } from '@heroicons/react/24/solid';
 
-const Empty = () => {
+import ImageEmpty from '../../public/images/Empty.svg';
+
+export default function EmptyJobsEmployer() {
   return (
     <div className="flex h-[71vh] flex-col items-center justify-center gap-y-6 pt-[71px] pb-40 md:gap-y-16 md:py-48">
       <Image
@@ -15,11 +18,14 @@ const Empty = () => {
       <p className="text-sm font-normal md:text-3xl">
         まだ求人が１件も登録されていません。
       </p>
-      <button className="mx-auto flex items-center justify-center gap-x-2 bg-pastelRed py-3 px-4 text-white hover:bg-jellyBean md:py-4 md:px-8 rounded-md md:rounded">
+
+      <Link
+        href="/jobs-employer/job-registration"
+        className="mx-auto flex items-center justify-center gap-x-2 rounded-md bg-pastelRed py-3 px-4 text-white hover:bg-jellyBean md:rounded md:py-4 md:px-8"
+      >
         <PlusIcon className="w-4 md:w-6" />
         <p className="text-sm font-normal md:text-lg">新規求人を登録する</p>
-      </button>
+      </Link>
     </div>
   );
-};
-export default Empty;
+}

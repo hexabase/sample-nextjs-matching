c8ef717143ca0c4d6b7f00c30c01f6403573898f
kondokentaro <kondokentaro@gmail.com>
2023-03-10 20:26:49 +0900

    add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head />
       <body>{children}</body>
     </html>

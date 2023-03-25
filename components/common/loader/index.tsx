aa0ad719f5da5fd6955665e8769e87b7f71dfc44
kondokentaro <kondokentaro@gmail.com>
2023-03-25 15:24:21 +0900

    fix bug

--- components/common/loader/index.tsx
+++ components/common/loader/index.tsx
@@ -1,11 +1,9 @@
-'use client';
-
 export default function Loader() {
   return (
     <>
-      <div class="flex items-center justify-center">
-        <div class="mt-12 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent text-red align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
-          <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+      <div className="flex items-center justify-center">
+        <div className="mt-12 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent text-red align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
             Loading...
           </span>
         </div>
